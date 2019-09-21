9721748922a59d42e758bf1fc01a3717edc2e87c
Chris Panican <chrispanican@gmail.com>
2019-09-21 00:35:40 -0400

    Pushed users.js

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,10 +1,11 @@
-  
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Users = sequelize.define('Users', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
+  const Users = sequelize.define('Users', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
     email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    phone: DataTypes.INTEGER
   }, {});
   Users.associate = function(models) {
     // associations can be defined here
