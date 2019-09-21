be34ad4ae4d36faef102ed9733b151b9e4f6935f
Chris Panican <chrispanican@gmail.com>
2019-09-21 06:42:48 -0400

    swap

--- frontend/src/components/orders/orders.js
+++ frontend/src/components/orders/orders.js
@@ -9,12 +9,12 @@ class Orders extends React.Component {
     return(
       <div>
         <h1 id="coffee-run-header" className="text-center">
-          {/* <Link to={{ pathname: '/placeorder' }}> */}
-            <a className="isDisabled"><i className="material-icons">keyboard_arrow_left</i>Coffee </a> 
-          {/* </Link> */}
-          <Link to={{ pathname: '/orders' }}>
-            <a>Run<i className="material-icons">keyboard_arrow_right</i></a>
+          <Link to={{ pathname: '/placeorder' }}>
+            <a ><i className="material-icons">keyboard_arrow_left</i>Coffee </a> 
           </Link>
+          {/* <Link to={{ pathname: '/orders' }}> */}
+            <a className="isDisabled">Run<i className="material-icons">keyboard_arrow_right</i></a>
+          {/* </Link> */}
         </h1>
         <OrdersRun/>
       </div>
