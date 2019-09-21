02eeca13acbf1f3fb6778e194721cd8d13c8f4e9
Shahzoda Davlatova <shahzoda01@gmail.com>
2019-09-21 08:22:41 -0400

    changed the store endpoint

--- backend/controllers/stores.js
+++ backend/controllers/stores.js
@@ -6,23 +6,12 @@ const StoresController = {
     const router = express.Router();
 
     router.get('/', this.index);
-    router.get('/stores', this.getStores);
-    // router.post('/', this.create);
-    // router.put('/:username', this.update);
-    // router.delete('/:username', this.delete);
 
     return router;
   },
 
   // List All users
   index(req, res) {
-    res.json({
-     msg: "Successful GET to '/stores' route"
-    });
-  },
-
-  // Get a username
-  getStores(req, res) {
     return models.Stores.findAll()
     .then(stores => {
       res.json(stores);
